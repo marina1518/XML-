87f3f5bc19146d68444a16d3935b5fac70910fbd
marina1518 <72987033+marina1518@users.noreply.github.com>
2021-07-15 22:29:46 +0200

    complete json function

--- jason.py
+++ jason.py
@@ -186,7 +186,208 @@ def jason_function(NODES_LIST,name_list):
                 bracket_start.remove(NODES_LIST[index][0].ht.split()[0])
 
             write = False
+        
+        # global child
+        # child = ''
+        # global enter_value
+        # enter_value = 0
+        # global value
+        # value = ''
+        # global itr
+        # itr = 0
+        #global length
+        global comma_counter
+        global FLAG_ERROR
+        if  NODES_LIST[index][1] != 'h' :
+         if (len(NODES_LIST[index][0].children) > 0 or ' ' in NODES_LIST[index][0].ht):
+            if ' ' in NODES_LIST[index][0].ht and NODES_LIST[index][1] == 'n':
+                json_data = json_data + '{' + '\n'
+                print('{')
+                split = NODES_LIST[index][0].ht.split()  # pointer refs="n05200169 n05616246">Attribute</pointer>
+                # print("SPLI:")
+                # print(split)
 
+                for j in range(0, len(split)):
+                    if (j > 0):  # id=1
+                        # print("J>0")
+                        itr = 0
+                        for k in split[j]:
+                            itr += 1
+                            # print("k:")
+                            # print(k)
+                            if (k != '='):
+                                if (enter_value == 0):
+                                    child += k
+                                else:
+                                    value += k
+                            else:
+                                json_data = json_data + spaces + '"' + '-' + child + '"' + ':'
+                                print(spaces + '"' + '-' + child + '"' + ':', end='')
+                                child = ''
+                                enter_value = 1
+
+                            if (itr == len(split[j])):
+                                if value.count('"') == 2 or value.count("'") == 2:
+                                    if '"' in value:
+                                        json_data = json_data + value + ',' + '\n'
+                                        print(value + ',')
+                                    else:
+                                        json_data = json_data + '"' + value + '"' + ',' + '\n'
+                                        print('"' + value + '"' + ',')
+
+                                    value = ''
+                                    enter_value = 0
+                                else:
+                                    value += ' '
+                                    pass
+                if (len(NODES_LIST[index][0].children) == 0):
+                    length = len(json_data)
+                    
+                    json_data = json_data[:length - 2]  ####DONE ','
+                    # print("after",json_data)
+                    json_data = json_data + '\n' + '}' + '\n'
+                    print('}')
+                if (len(NODES_LIST[index][0].children) > 0):
+                    FLAG_ERROR = 1
+                    for E in NODES_LIST[index][0].children:
+
+                        if E.ht in name_list:
+                            FLAG_ERROR = 0
+                            
+                            break
+                    if FLAG_ERROR != 0:
+                        for j in NODES_LIST[index][0].children:
+                            ERROR_v.append(j.ht)
+                else:
+                    FLAG_ERROR = 0
+                    
+
+            else:
+                # FLAG_ERROR = 1
+                ####### MOSH H 
+                
+                  for j in NODES_LIST[index][0].children:
+                     # if NODES_LIST[index][0].children[j][1]== 'n':
+                    if j.ht in name_list:
+                        # FLAG_ERROR = 0
+                        json_data = json_data + "{" + '\n'
+                        print("{")
+                        break
+                
+
+        if NODES_LIST[index][0].children:
+            level += 1
+
+        for j in NODES_LIST[index][0].children:
+            new = j.ht
+            print_json, write = xmltojson(new, level, count, write, json_data)
+            json_data = print_json
+
+        if NODES_LIST[i][1] == 'h':
+             for T in NODES_LIST[index][0].children:
+                        if '!' in T.ht or '?' in T.ht or '/' in T.ht :
+                            repeated.remove(T.ht)
+                        else :
+                            repeated.remove(T.ht.split()[0])
+
+        if NODES_LIST[i][1] == 'n':
+            for j in NODES_LIST[i][0].children:
+                # if NODES_LIST[index][0].children[j][1]== 'n':
+                if j.ht in name_list:
+                    level -= 1
+                    spaces = ' ' * level
+                    length = len(json_data)
+                    comma_counter = 0
+                    for new in range(len(json_data) - 1, -1, -1):
+                        comma_counter += 1
+                        if (json_data[new] == ']' or json_data[new] == '}'):
+                            break
+                        if (json_data[new] == ','):
+                            json_data = json_data[:length - comma_counter]
+                            comma_counter = 0
+                            break
+
+                    if NODES_LIST[i][0].ht in last_child and NODES_LIST[i][0].ht not in duplicate:
+                        json_data = json_data + spaces + "}" + '\n'
+                        print(spaces + "}")
+                    else:
+                        json_data = json_data + spaces + "}" + ',' + '\n'
+                        print(spaces + "}" + ',')
+                    count_rep = 0
+                    for j in NODES_LIST[index][0].children:
+                        trial.append(j.ht.split()[0])
+                        count_rep = trial.count(j.ht.split()[0])
+                        if (count_rep > 1):
+                            duplicate.remove(j.ht.split()[0])
+                            # repeated.remove(j.ht.split()[0])
+                    #print ("#THE PARENT")        
+                    #print(NODES_LIST[index][0].ht)        
+                    for o in NODES_LIST[index][0].children:
+                        #print ("CHILDREN:")
+                        #print (o.ht) 
+                        if '!' in o.ht or '?' in o.ht or 'special/' in o.ht :
+                            repeated.remove(o.ht)
+                        else :
+                            #print ("REPEATED")
+                            #print(repeated)
+                            if (o.ht in name_list) :
+                              repeated.remove(o.ht.split()[0])
+
+                    # print(""""REPEATED"""" ")
+                    # print(repeated)
+                    trial.clear()
+                    # repeated.clear()
+
+                    break
+
+            if (NODES_LIST[i][0].ht.split()[0] in duplicate):
+                # bracket_end.remove(NODES_LIST[i][0].ht)
+                # counter_end=bracket_end.count(NODES_LIST[i][0].ht) +1 #its number in bracket list
+                if (bracket_end.count(NODES_LIST[i][0].ht.split()[0]) + 1 == 1):
+                    level -= 1
+                    spaces = ' ' * level
+                    length = len(json_data)
+                    comma_counter = 0
+                    for new in range(len(json_data) - 1, -1, -1):
+                        comma_counter += 1
+                        if (json_data[new] == ']' or json_data[new] == '}'):
+                            break
+                        if (json_data[new] == ','):
+                            json_data = json_data[:length - comma_counter]
+                            comma_counter = 0
+                            break
+                    # json_data=json_data+spaces+"]"+'\n'
+                    if NODES_LIST[i][0].ht in last_child:
+                        json_data = json_data + spaces + "]" + '\n'
+                        print(spaces + ']')
+                    else:
+                        json_data = json_data + spaces + "]" + ',' + '\n'
+                        print(spaces + ']' + ',')
+                else:
+                    bracket_end.remove(NODES_LIST[i][0].ht.split()[0])
+        return json_data, write
+
+    #####################################################################################
+
+    root = NODES_LIST[0][0].ht
+    # print ("////////////////////////////////////////////////////////////")
+    # print_xml (root) ######## ROOT
+    print("////////////////////////////////////////////////////////////")
+    json_file, flag = xmltojson(root, 1, 0, write, '{' + '\n')
+    json_file += '}'
+    print("file")
+    print(json_file)
+    #myfile = open('E:/3rd cse/sec term/data structure/PROJECT/convert.json', "w")
+
+    # myfile.write("ERROR NOMBER:"+str(counter_error)+"\n")
+
+    #myfile.write(json_file)
+
+    file3 = open('jason.json', 'w')
+    file3.write(json_file)
+    file3.close()
+
+   
 
 
 
